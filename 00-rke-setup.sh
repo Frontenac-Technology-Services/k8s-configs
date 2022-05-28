6a7b1a74042158db69839d7fa3a29b1251381046
Noah Burrell <noah.burrell@empire.ca>
2022-05-28 16:21:46 -0400

    update

--- 00-rke-setup.sh
+++ 00-rke-setup.sh
@@ -4,4 +4,4 @@ BASEDIR=.rke
 mkdir $BASEDIR
 wget https://github.com/rancher/rke/releases/download/v1.3.11/rke_linux-amd64 -O $BASEDIR/rke
 chmod +x $BASEDIR/rke
-./$BASEDIR/rke up --config $BASEDIR/cluster.yml
+./$BASEDIR/rke up --config $BASEDIR/cluster.yml --disable-port-check
