9b88bb009c23c5f389ed693dbcdd363f4a03bb58
Noah Burrell <noah.burrell@empire.ca>
2022-05-28 00:07:07 -0400

    initial

---
+++ 00-rke-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+BASEDIR=.rke
+
+mkdir $BASEDIR
+wget https://github.com/rancher/rke/releases/download/v1.3.11/rke_linux-amd64 -O $BASEDIR/rke
+chmod +x $BASEDIR/rke
+./$BASEDIR/rke up --config $BASEDIR/cluster.yml
